0b8f9ff0f969ecd7586caeee3fd0b16dc938a0a5
charleslomboni <charleslomboni@gmail.com>
2024-06-24 19:06:19 -0300

    Add the exercise SystemInformation

--- exercises/ex_SystemInformation/ex_SystemInformation.cpp
+++ exercises/ex_SystemInformation/ex_SystemInformation.cpp
@@ -1,8 +1,14 @@
 // ex_SystemInformation.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#define SECURITY_WIN32						// To use GetComputerObjectName
 #include <windows.h>
-#include<stdio.h>
+#include <stdio.h>
+
+#include <security.h>						// To use GetComputerObjectName
+#include <secext.h>							// To use GetComputerObjectName
+
+#pragma comment(lib, "Secur32.lib")			// To use GetComputerObjectName
 
 int main()
 {
@@ -10,7 +16,7 @@ int main()
 	// GetComputerName
 	// GetWindowsDirectory
 	// QueryPerformanceCounter
-	// GetPrductInfo
+	// GetProductInfo
 	// GetComputerObjectName
 	// Handl errors if they occur
 
@@ -47,5 +53,51 @@ int main()
 	if (wdReturn) {
 		printf("\tWindows Directory            : %ws\n\n", winPath);
 	}
+	// =========================================================================================
+	printf("[+] QueryPerformanceCounter\n");
+
+	LARGE_INTEGER performanceCount;
+	BOOL pfReturn = ::QueryPerformanceCounter(&performanceCount);
+
+	if (pfReturn) {
+		printf("\tQuery Performance Counter    : %llu\n\n", performanceCount.QuadPart);
+	}
+	// =========================================================================================
+	printf("[+] GetProductInfo\n");
+
+	/*BOOL GetProductInfo(
+		[in]  DWORD  dwOSMajorVersion,
+		[in]  DWORD  dwOSMinorVersion,
+		[in]  DWORD  dwSpMajorVersion,
+		[in]  DWORD  dwSpMinorVersion,
+		[out] PDWORD pdwReturnedProductType
+	);*/
+
+	DWORD  dwOSMajorVersion{ 6 };
+	DWORD  dwOSMinorVersion{ 0 };
+	DWORD  dwSpMajorVersion{ 0 };
+	DWORD  dwSpMinorVersion{ 0 };
+	DWORD pdwReturnedProductType{ 0 };
+
+	BOOL gpiReturn = ::GetProductInfo(dwOSMajorVersion, dwOSMinorVersion, dwSpMajorVersion, dwSpMinorVersion, &pdwReturnedProductType);
+
+	if (gpiReturn) {
+		printf("\tProduct Info                 : %d\n\n", pdwReturnedProductType);
+	}
+	// =========================================================================================
+	printf("[+] GetComputerObjectName\n");
+
+	WCHAR lpNameBuffer[50];
+	ULONG nSize = sizeof(lpNameBuffer);
+
+	// https://learn.microsoft.com/en-us/windows/win32/api/secext/ne-secext-extended_name_format
+	BOOLEAN gconResult = ::GetComputerObjectName(NameDisplay, lpNameBuffer, &nSize);
+	if (gconResult) {
+		printf("\tComputer Object Name         : %d\n\n", lpNameBuffer);
+	}
+	else {
+		printf("\tGetLastErrorCode             : 0x%x\n\n", ::GetLastError());
+	}
+
 	return 0;
 }
