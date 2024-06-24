2503c41793f0a980c6187cde89a38f62f44eec26
charleslomboni <charleslomboni@gmail.com>
2024-06-24 14:13:48 -0300

    Add comment to the KUSER_SHARED_DATA

--- src/ex_KUSER_SHARED_DATA/ex_KUSER_SHARED_DATA.cpp
+++ src/ex_KUSER_SHARED_DATA/ex_KUSER_SHARED_DATA.cpp
@@ -6,6 +6,12 @@
 
 int main()
 {
+	// https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/ntddk/ns-ntddk-kuser_shared_data
+	// KUSER_SHARED_DATA is mapped to every process
+	// to the same virtual address [0x7FFE0000]
+	//
+	// https://www.geoffchappell.com/studies/windows/km/ntoskrnl/inc/api/ntexapi_x/kuser_shared_data/index.htm
+	// The read-only user-mode address for the shared data is 0x7FFE0000, both in 32-bit and 64-bit Windows.
 	auto sharedUserData = (BYTE*)0x7FFE0000;
 
 	printf("Version: %d.%d.%d\n",
